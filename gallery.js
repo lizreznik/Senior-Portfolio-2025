04d4ba21560e6e42d4a8a2259bc7a163112dd4d7
lizreznik <525ereznik@frhsd.com>
2025-05-12 11:53:50 -0400

    using github projects finish working on gallery - resume page started day 3

--- gallery.js
+++ gallery.js
@@ -1,14 +1,43 @@
-const vue_app = Vue.createApp({
-      created() {
-            fetch('gallery.json').then(response => response.json()).then(json => {
-                  this.gallery = json
-            })
+const projectCards = [
+      {
+            title: "project 1",
+            poster: "...",
+            year: "...",
       },
-      data() {
-            return {
-                  gallery: [],
-            }
-      }
+];
+function generateGalleryCards() {
+      const galleryCardsContainer = document.getElementById("projectCards")
 
-})
-vue_app.mount("#vue_app")
+      projectCards.forEach(project => {
+            const card = document.createElement('div')
+            card.classList.add('col-md-4')
+
+            // styling card based on position: 
+            let backgroundColor
+
+            switch (project.genre.toLowerCase) {
+                  case 'forward':
+                        backgroundColor = '#ffc107'
+                        break
+            }
+            card.style.backgroundColor = backgroundColor
+            //create a list of skills with <li> tags //
+            const favoriteList = member.favorite.map(favorite => `<li> ${favorite} </li> `).join('')
+
+            card.innerHTML = `
+    <div class = "card"> 
+        <div class = "card-header"> ${project.title} </div> \
+        <div class = "h1"> ${project.year} </div> 
+        <div class = "card-body"> 
+            <div class = "card-img">
+                <img class="img-fluid" src= ${project.poster}
+            </div> 
+        </div> 
+    </div> 
+    `
+
+            teamCardsContainer.appendChild(card)
+      })
+}
+
+window.onload = generateTeamCards()
